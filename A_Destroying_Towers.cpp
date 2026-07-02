909dd1e7c0450465180ff12a099b9c4a695e3cc9
imrajeevraj <rajeev.ranjan948@hotmail.com>
2026-07-03 02:03:01 +0530

    Daily update: 2026-07-03 02:03:01

---
+++ A_Destroying_Towers.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int t;
+	cin >> t; 
+	while (t--)
+	{
+		int n;
+		cin >> n; 
+		long long sum = 0;
+        for (int i = 0; i < n ; i++){
+            int m;
+            cin >> m ;
+            if()
+        }
+		while (num >= 0) 
+		{
+			ans.push_back(num);
+			num--;
+		}
+		num = pow(2, msb); 
+		while (num <= n) 
+		{
+			ans.push_back(num);
+			num++;
+		}
+
+		for (auto it : ans) 
+			cout << it << " ";
+		cout << endl;
+	}
+}
+
+
