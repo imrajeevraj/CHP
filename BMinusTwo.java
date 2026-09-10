2fd03afde505c4fb525681cc15e5bf16d98630bb
imrajeevraj <rajeev.ranjan948@hotmail.com>
2026-09-10 09:20:37 +0530

    Daily update: 2026-09-10 09:20:37

---
+++ BMinusTwo.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class BMinusTwo {
+   public static void main(String[] args){
+        Scanner sc =  new Scanner(System.in);
+        int t =  sc.nextInt();
+
+        while(t-- > 0){
+            int n =  sc.nextInt();
+            int ans = 0;
+            int temp = 0;
+            boolean first = true;
+            for(int i = 0; i < n; i++){
+                int k =  sc.nextInt();
+                int res = Math.abs(k - 2);
+            }
+            System.out.println(ans);
+        }
+   } 
+}
